062dc5fcab69fa05e07b23ba9cad2a441f5ee00e
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-09-09 12:26:46 +0100

    add collision file and setup parts of the module

--- src/pong/physics/collision.js
+++ src/pong/physics/collision.js
@@ -1,7 +1,37 @@
+const CollisionStrategies = {
+
+    BOX:0,
+    CIRCLE:1
+};
+
 class Collision {
 
-    constructor(){
+    constructor(){}
 
+    add(strategy,...spriteObject){
+
+        if(strategy == CollisionStrategies.BOX){
+
+            spriteObject.forEach(sprite => {
+     
+                
+            });
+        }
+        
+        if(strategy == CollisionStrategies.CIRCLE){
+            
+            spriteObject.forEach(sprite => {
+
+                        
+            });
+        }
+
+        return this;
+    }
+
+    hitBox(){
+
+        
     }
 }
 
